2d197d03ff1407832fd8c9dd32c936ca03efd79e
Auto Commit <action@local>
2026-01-31 00:58:29 +0530

    modified:   server/index.js

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../code-collaborator/build')));
 
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, '../code-collaborator/build', 'index.html'));
 });
 
